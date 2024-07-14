d221199bb515963836cfebaee34956238cd58974
ish2k <ishaa.gupta04@hotmail.com>
2024-07-14 21:30:56 +0530

    updates

--- app.py
+++ app.py
@@ -3,24 +3,29 @@ import random
 import json
 import os
 import time
+import pandas as pd
 
 st.title('GRE Word Practice')
 
-with open("vocab.json", "r", encoding="utf-8") as f:
-    words = json.load(f)
+df = pd.read_csv("vocab.csv")
+df.columns = ["word", "meaning", "example"]
+obj = None
 
 def get_random_word():
-    word = random.choice(list(words.keys()))
-    return word
+    global obj
+    rand_int = random.randint(0, len(df))
+    obj = df.iloc[rand_int]
+
+    return obj.word
 
 def get_answer(word):
-    ans = words[word]
+    global obj
 
     with placeholder.container():
         st.write('Answer is:')
-        st.write(f'Word: {word}')
-        st.write(f'Meaning: {ans["meaning"]}')
-        st.write(f'Example: {ans["example"]}')
+        st.write(f'Word: {obj.word}')
+        st.write(f'Meaning: {obj.meaning}')
+        st.write(f'Example: {obj.example}')
 
 placeholder = st.empty()
 
@@ -34,3 +39,4 @@ with placeholder.container():
 
 with st.form(key='answer'):
     get_answer_button = st.form_submit_button('Get Answer', on_click=get_answer, args=(word,))
+
